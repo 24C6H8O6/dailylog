1444369e81372b9833f4cc7ce6e9daf9c7a3391e
CC6H8O6 <108849182+Yoonseungwook@users.noreply.github.com>
2022-11-22 11:29:33 +0900

    Add files via upload
    11/22

--- router.js
+++ router.js
@@ -1,10 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const mysql = require("mysql");
+const morgan = require("morgan");
+const app = express();
 
-var conn = mysql.createConnection({
-  host: "database-2.czhwbelwlp5g.ap-northeast-2.rds.amazonaws.com",
-  user: "admin",
-  password: "121104115",
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const body_parser = require("body-parser");
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({ extended: false }));
+
+const path = require("path");
+app.set("view engine", "ejs");
+// 경로 설정
+app.set("views", __dirname + "/views");
+// app.use(express.static(__dirname + "./views"));
+
+// const conn = mysql.createConnection({
+//   host: "database-2.cto6iphlk0yd.ap-northeast-2.rds.amazonaws.com",
+//   user: "admin2",
+//   password: "121104115",
+//   port: "3306",
+//   database: "auto_farming",
+// });
+
+const conn = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "test",
+  password: "*swy7751",
   port: "3306",
   database: "auto_farming",
 });
@@ -24,7 +48,7 @@ router.get("/", function (req, res) {
 router.get("/response", function (request, response) {
   console.log("사용자가 보낸 값 : " + request.query.site);
 
-  response.redirect("http://192.168.70.56:5501/smhrd/express/public/ex04.html");
+  response.redirect("http://127.0.0.1:5501/smhrd/express/public/ex04.html");
   // 외부/내부 해당페이지로 이동
 
   // 네이버/구글/다음 사용자가 선택한 값으로 사이트를 이동시키시오.
@@ -35,8 +59,7 @@ router.post("/post", function (request, response) {
   console.log("사용자가 보낸 값 : " + request.body.text);
 });
 
-var mysql = require("mysql"); // 설치한 mysql기능
-const { response } = require("express");
+// const { response } = require("express");
 // 사용자가 보낸 값이 post방식일 때 분석해주는 express기능
 
 // Join 기능구현하기
@@ -57,12 +80,6 @@ router.post("/Join", function (request, response) {
       console.log("입력실패" + err);
     }
   });
-});
-
-// test 해봄
-router.post("/test", function (request, response) {
-  let id = request.body.id;
-  console.log("yes");
 });
 
 // Delete 기능구현하기
@@ -162,6 +179,17 @@ router.post("/OneSelect", function (request, response) {
     }
   });
 });
+
+router.get("/sound/:name", (req, res) => {
+  const { name } = req.params;
+  if (name == "dog") {
+    res.json({ sound: "멍멍" });
+  } else if (name == "cat") {
+    res.json({ sound: "야옹" });
+  } else {
+    res.json({ sound: "알수없음" });
+  }
+});
 // ------------------------------------------------------------------------------------------------------------------
 // ------------------------------------------------------------------------------------------------------------------
 // ------------------------------------------------------------------------------------------------------------------
@@ -197,69 +225,103 @@ router.post("/OneSelect", function (request, response) {
 router.post("/Login", function (request, response) {
   let id = request.body.id;
   let pw = request.body.pw;
-  let sql = "select * from user_table where user_id = ? and user_pw = ?";
-
+  let sql = "select * from user_table where user_id = ? and user_pw = ? ";
   conn.query(sql, [id, pw], function (err, rows) {
-    // rows.length
+    // rows.length;
     if (rows.length > 0) {
       console.log("로그인 성공 : " + rows.length);
-      response.redirect(
-        "http://127.0.0.1:5501/smhrd/express/public/ex06S.html"
-      );
+      // response.redirect(
+      //   "http://127.0.0.1:5501/smhrd/express/public/ex06S.html"
+      // );
     } else {
       console.log("로그인 실패 : " + rows.length);
-      response.redirect(
-        "http://127.0.0.1:5501/smhrd/express/public/ex06F.html"
-      );
+      // response.redirect(
+      //   "http://127.0.0.1:5501/smhrd/express/public/ex06F.html"
+      // );
     }
+    // console.log(rows);
   });
+  // login(id, pw);
 });
+
+// function login(user_id, user_pw) {
+//   conn.connect();
+//   let sql = "select * from user_table where user_id = ? and user_pw = ?";
+//   conn.query(sql, [user_id, user_pw], function (err, rows) {
+//     // rows.length
+//     if (rows.length > 0) {
+//       console.log("로그인 성공 : " + rows.length);
+//       response.redirect(
+//         "http://127.0.0.1:5501/smhrd/express/public/ex06S.html"
+//       );
+//       // alert("성공");
+//     } else {
+//       console.log("로그인 실패 : " + rows.length);
+//       response.redirect(
+//         "http://127.0.0.1:5501/smhrd/express/public/ex06F.html"
+//       );
+//       // alert("실패");
+//     }
+//   });
+// }
 
 // get방식 센서데이터(farm_id 수정 필요)
 router.get("/insertSensor", function (req, res) {
   console.log("사용자가 보낸 값 : " + req.query.temp);
   console.log("사용자가 보낸 값 : " + req.query.humd);
   console.log("사용자가 보낸 값 : " + req.query.height);
+  console.log("사용자가 보낸 값 : " + req.query.robotheight);
   console.log("사용자가 보낸 값 : " + req.query.co2);
-  let temp = req.query.temp;
-  let humd = req.query.humd;
-  let height = req.query.height;
-  let co2 = req.query.co2;
-  let sql =
-    "insert into sensor_table(sensor_time, farm_id, tem_sensor, hum_sensor, wtuw_sensor, rbuw_sensor, co2_sensor) values(now(),'123',?,?,?,'1',?)";
+  let tempi = req.query.temp;
+  let humdi = req.query.humd;
+  let heighti = req.query.height;
+  let robotheighti = req.query.robotheight;
+  let co2i = req.query.co2;
 
-  conn.query(sql, [temp, humd, height, co2], function (err, rows) {
-    if (!err) {
-      console.log("입력성공");
-    } else {
-      console.log("입력실패" + err);
+  let sql =
+    "insert into sensor_table(sensor_time, farm_id, tem_sensor, hum_sensor, wtuw_sensor, rbuw_sensor, co2_sensor) values(now(),'123',?,?,?,?,?)";
+
+  conn.query(
+    sql,
+    [tempi, humdi, heighti, robotheighti, co2i],
+    function (err, rows) {
+      if (!err) {
+        console.log("입력성공");
+      } else {
+        console.log("입력실패" + err);
+      }
     }
-  });
+  );
 });
 
 // DB에서 가장 최신의 센서 데이터 가져오기(가져온 값들 밖으로 빼기)
 router.get("/selectSensor", function (req, res) {
   // let sql = "select temp, humd, wtuw, rbuw, co2 from sensor_table";
-  conn.connect();
   let sql =
     "select * from (select * from sensor_table)a order by sensor_time desc limit 1;";
+
   conn.query(sql, function (err, rows) {
+    //console.log(rows);
     if (!err) {
       console.log("조회성공");
-      console.log(rows[0].wtuw_sensor);
-      tem = rows[0].tem_sensor;
-      var humd = rows[0].humd_sensor;
-      var wtuw = rows[0].wtuw_sensor;
-      var rbuw = rows[0].rbuw_sensor;
-      var co2 = rows[0].co2_sensor;
-      document.getElementById("data").innerHTML = tem;
-      return tem, humd, wtuw, rbuw, co2;
+      console.log(rows[0].tem_sensor);
+      let tems = rows[0].tem_sensor;
+      let humds = rows[0].hum_sensor;
+      let wtuws = rows[0].wtuw_sensor;
+      let rbuws = rows[0].rbuw_sensor;
+      let co2s = rows[0].co2_sensor;
+      res.render("index.ejs", {
+        tem: tems,
+        humd: humds,
+        wtuw: wtuws,
+        rbuw: rbuws,
+        co2: co2s,
+      });
     } else {
       console.log("조회실패 : " + err);
     }
   });
 });
-var tem = tem;
 
 // 액츄에이터 작동여부 수신 후 DB에 업데이트
 router.post("/updateAct", function (req, res) {
@@ -307,108 +369,11 @@ router.post("/updateAct", function (req, res) {
 // btn1~btn9 : 물통밸브, 급수밸브, 에어컨적외선, 산소수, 환기적외선, 컨베이어, LED1, LED2, 로봇
 var a1;
 
-function btn1(a) {
-  a1 = string(a.checked);
-  if (a.checked == true) {
-    console.log(a.checked);
-  } else {
-    console.log(a.checked);
-  }
-}
-
 router.get("/test1", function (require, response) {
   // toggle();
   response.send(a1);
   console.log(a1);
 });
-
-function btn2(a) {
-  this.a = a;
-  if (a.checked == true) {
-    console.log(a.checked);
-    // let gg = "true";
-  } else {
-    console.log(a.checked);
-    // let gg = "false";
-  }
-}
-
-function btn3(a) {
-  this.a = a;
-  if (a.checked == true) {
-    console.log(a.checked);
-    // let gg = "true";
-  } else {
-    console.log(a.checked);
-    // let gg = "false";
-  }
-}
-
-function btn4(a) {
-  this.a = a;
-  if (a.checked == true) {
-    console.log(a.checked);
-    // let gg = "true";
-  } else {
-    console.log(a.checked);
-    // let gg = "false";
-  }
-}
-
-function btn5(a) {
-  this.a = a;
-  if (a.checked == true) {
-    console.log(a.checked);
-    // let gg = "true";
-  } else {
-    console.log(a.checked);
-    // let gg = "false";
-  }
-}
-
-function btn6(a) {
-  this.a = a;
-  if (a.checked == true) {
-    console.log(a.checked);
-    // let gg = "true";
-  } else {
-    console.log(a.checked);
-    // let gg = "false";
-  }
-}
-
-function btn7(a) {
-  this.a = a;
-  if (a.checked == true) {
-    console.log(a.checked);
-    // let gg = "true";
-  } else {
-    console.log(a.checked);
-    // let gg = "false";
-  }
-}
-
-function btn8(a) {
-  this.a = a;
-  if (a.checked == true) {
-    console.log(a.checked);
-    // let gg = "true";
-  } else {
-    console.log(a.checked);
-    // let gg = "false";
-  }
-}
-
-function btn9(a) {
-  this.a = a;
-  if (a.checked == true) {
-    console.log(a.checked);
-    // let gg = "true";
-  } else {
-    console.log(a.checked);
-    // let gg = "false";
-  }
-}
 
 module.exports = router;
 // router에 대한 정보를 갖고있는 객체를 외부에서 사용할 수 있게 모듈화
---
+++ router2.js
@@ -0,0 +1,96 @@
+function btn1(a) {
+  a1 = string(a.checked);
+  if (a.checked == true) {
+    console.log(a.checked);
+  } else {
+    console.log(a.checked);
+  }
+}
+
+function btn2(a) {
+  this.a = a;
+  if (a.checked == true) {
+    console.log(a.checked);
+    // let gg = "true";
+  } else {
+    console.log(a.checked);
+    // let gg = "false";
+  }
+}
+
+function btn3(a) {
+  this.a = a;
+  if (a.checked == true) {
+    console.log(a.checked);
+    // let gg = "true";
+  } else {
+    console.log(a.checked);
+    // let gg = "false";
+  }
+}
+
+function btn4(a) {
+  this.a = a;
+  if (a.checked == true) {
+    console.log(a.checked);
+    // let gg = "true";
+  } else {
+    console.log(a.checked);
+    // let gg = "false";
+  }
+}
+
+function btn5(a) {
+  this.a = a;
+  if (a.checked == true) {
+    console.log(a.checked);
+    // let gg = "true";
+  } else {
+    console.log(a.checked);
+    // let gg = "false";
+  }
+}
+
+function btn6(a) {
+  this.a = a;
+  if (a.checked == true) {
+    console.log(a.checked);
+    // let gg = "true";
+  } else {
+    console.log(a.checked);
+    // let gg = "false";
+  }
+}
+
+function btn7(a) {
+  this.a = a;
+  if (a.checked == true) {
+    console.log(a.checked);
+    // let gg = "true";
+  } else {
+    console.log(a.checked);
+    // let gg = "false";
+  }
+}
+
+function btn8(a) {
+  this.a = a;
+  if (a.checked == true) {
+    console.log(a.checked);
+    // let gg = "true";
+  } else {
+    console.log(a.checked);
+    // let gg = "false";
+  }
+}
+
+function btn9(a) {
+  this.a = a;
+  if (a.checked == true) {
+    console.log(a.checked);
+    // let gg = "true";
+  } else {
+    console.log(a.checked);
+    // let gg = "false";
+  }
+}
